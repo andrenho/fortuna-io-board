90951319b3a8cb03675bbc6d6d8aa0181cd3baff
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-08-19 06:58:04 -0500

    .

--- emulator/library/fortuna.c
+++ emulator/library/fortuna.c
@@ -1,5 +1,17 @@
 #include "fortuna.h"
 
+#include "raylib.h"
+
 void fortuna_init(uint16_t event_queue_size, void (*core1_step_function)())
 {
+    InitWindow(1280, 960, "fortuna-io-board emulator");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
